a1a5b6b11e29d3304a48ee26255518421cf5e18f
Siwan841227 <aapka-email@example.com>
2025-09-11 18:03:03 +0530

    new

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://interview-cochhh.onrender.com/api", // ✅ Deployed backend URL
 });
 
 // Agar token hai to auth header me bhejo
