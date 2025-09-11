7f4bd7e80a6d995735c59186ca56a4bcab05a0bc
Siwan841227 <aapka-email@example.com>
2025-09-11 14:47:53 +0530

    chore: clean install for Vercel

---
+++ frontend/dist/assets/ProtectedRoute-bvBimNce.js
@@ -0,0 +1,1 @@
+import{R as t,N as r}from"./index-DMsRDNzL.js";function u({children:e}){return JSON.parse(localStorage.getItem("user"))?e:t.createElement(r,{to:"/auth",replace:!0})}export{u as default};
